1c525efd96988ee0a1db6868a92357e0c1c56f8b
Daniel Weiss <mail@weiss-daniel.de>
2015-03-30 22:42:49 +0200

    lesson 40

--- SDL2_Tutorials/main.cpp
+++ SDL2_Tutorials/main.cpp
@@ -1,40 +1,15 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2015)
 and may not be redistributed without written permission.*/
 
-//Using SDL, SDL_image, standard IO, strings, and file streams
+//Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
-#include <fstream>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-//The dimensions of the level
-const int LEVEL_WIDTH = 1280;
-const int LEVEL_HEIGHT = 960;
-
-//Tile constants
-const int TILE_WIDTH = 80;
-const int TILE_HEIGHT = 80;
-const int TOTAL_TILES = 192;
-const int TOTAL_TILE_SPRITES = 12;
-
-//The different tile sprites
-const int TILE_RED = 0;
-const int TILE_GREEN = 1;
-const int TILE_BLUE = 2;
-const int TILE_CENTER = 3;
-const int TILE_TOP = 4;
-const int TILE_TOPRIGHT = 5;
-const int TILE_RIGHT = 6;
-const int TILE_BOTTOMRIGHT = 7;
-const int TILE_BOTTOM = 8;
-const int TILE_BOTTOMLEFT = 9;
-const int TILE_LEFT = 10;
-const int TILE_TOPLEFT = 11;
 
 //Texture wrapper class
 class LTexture
@@ -73,90 +48,31 @@ public:
 	int getWidth();
 	int getHeight();
 
+	//Pixel manipulators
+	bool lockTexture();
+	bool unlockTexture();
+	void* getPixels();
+	int getPitch();
+
 private:
 	//The actual hardware texture
 	SDL_Texture* mTexture;
+	void* mPixels;
+	int mPitch;
 
 	//Image dimensions
 	int mWidth;
 	int mHeight;
 };
 
-//The tile
-class Tile
-{
-public:
-	//Initializes position and type
-	Tile(int x, int y, int tileType);
-
-	//Shows the tile
-	void render(SDL_Rect& camera);
-
-	//Get the tile type
-	int getType();
-
-	//Get the collision box
-	SDL_Rect getBox();
-
-private:
-	//The attributes of the tile
-	SDL_Rect mBox;
-
-	//The tile type
-	int mType;
-};
-
-//The dot that will move around on the screen
-class Dot
-{
-public:
-	//The dimensions of the dot
-	static const int DOT_WIDTH = 20;
-	static const int DOT_HEIGHT = 20;
-
-	//Maximum axis velocity of the dot
-	static const int DOT_VEL = 10;
-
-	//Initializes the variables
-	Dot();
-
-	//Takes key presses and adjusts the dot's velocity
-	void handleEvent(SDL_Event& e);
-
-	//Moves the dot and check collision against tiles
-	void move(Tile *tiles[]);
-
-	//Centers the camera over the dot
-	void setCamera(SDL_Rect& camera);
-
-	//Shows the dot on the screen
-	void render(SDL_Rect& camera);
-
-private:
-	//Collision box of the dot
-	SDL_Rect mBox;
-
-	//The velocity of the dot
-	int mVelX, mVelY;
-};
-
 //Starts up SDL and creates window
 bool init();
 
 //Loads media
-bool loadMedia(Tile* tiles[]);
+bool loadMedia();
 
 //Frees media and shuts down SDL
-void close(Tile* tiles[]);
-
-//Box collision detector
-bool checkCollision(SDL_Rect a, SDL_Rect b);
-
-//Checks collision box against set of tiles
-bool touchesWall(SDL_Rect box, Tile* tiles[]);
-
-//Sets tiles from tile map
-bool setTiles(Tile *tiles[]);
+void close();
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -165,9 +81,7 @@ SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
 //Scene textures
-LTexture gDotTexture;
-LTexture gTileTexture;
-SDL_Rect gTileClips[TOTAL_TILE_SPRITES];
+LTexture gFooTexture;
 
 LTexture::LTexture()
 {
@@ -175,6 +89,8 @@ LTexture::LTexture()
 	mTexture = NULL;
 	mWidth = 0;
 	mHeight = 0;
+	mPixels = NULL;
+	mPitch = 0;
 }
 
 LTexture::~LTexture()
@@ -199,20 +115,39 @@ bool LTexture::loadFromFile(std::string path)
 	}
 	else
 	{
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
-
-		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-		if (newTexture == NULL)
+		//Convert surface to display format
+		SDL_Surface* formattedSurface = SDL_ConvertSurface(loadedSurface, SDL_GetWindowSurface(gWindow)->format, NULL);
+		if (formattedSurface == NULL)
 		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+			printf("Unable to convert loaded surface to display format! SDL Error: %s\n", SDL_GetError());
 		}
 		else
 		{
-			//Get image dimensions
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
+			//Create blank streamable texture
+			newTexture = SDL_CreateTexture(gRenderer, SDL_GetWindowPixelFormat(gWindow), SDL_TEXTUREACCESS_STREAMING, formattedSurface->w, formattedSurface->h);
+			if (newTexture == NULL)
+			{
+				printf("Unable to create blank texture! SDL Error: %s\n", SDL_GetError());
+			}
+			else
+			{
+				//Lock texture for manipulation
+				SDL_LockTexture(newTexture, NULL, &mPixels, &mPitch);
+
+				//Copy loaded/formatted surface pixels
+				memcpy(mPixels, formattedSurface->pixels, formattedSurface->pitch * formattedSurface->h);
+
+				//Unlock texture to update
+				SDL_UnlockTexture(newTexture);
+				mPixels = NULL;
+
+				//Get image dimensions
+				mWidth = formattedSurface->w;
+				mHeight = formattedSurface->h;
+			}
+
+			//Get rid of old formatted surface
+			SDL_FreeSurface(formattedSurface);
 		}
 
 		//Get rid of old loaded surface
@@ -270,6 +205,8 @@ void LTexture::free()
 		mTexture = NULL;
 		mWidth = 0;
 		mHeight = 0;
+		mPixels = NULL;
+		mPitch = 0;
 	}
 }
 
@@ -317,133 +254,58 @@ int LTexture::getHeight()
 	return mHeight;
 }
 
-Tile::Tile(int x, int y, int tileType)
+bool LTexture::lockTexture()
 {
-	//Get the offsets
-	mBox.x = x;
-	mBox.y = y;
+	bool success = true;
 
-	//Set the collision box
-	mBox.w = TILE_WIDTH;
-	mBox.h = TILE_HEIGHT;
-
-	//Get the tile type
-	mType = tileType;
-}
-
-void Tile::render(SDL_Rect& camera)
-{
-	//If the tile is on screen
-	if (checkCollision(camera, mBox))
+	//Texture is already locked
+	if (mPixels != NULL)
 	{
-		//Show the tile
-		gTileTexture.render(mBox.x - camera.x, mBox.y - camera.y, &gTileClips[mType]);
+		printf("Texture is already locked!\n");
+		success = false;
 	}
-}
-
-int Tile::getType()
-{
-	return mType;
-}
-
-SDL_Rect Tile::getBox()
-{
-	return mBox;
-}
-
-Dot::Dot()
-{
-	//Initialize the collision box
-	mBox.x = 0;
-	mBox.y = 0;
-	mBox.w = DOT_WIDTH;
-	mBox.h = DOT_HEIGHT;
-
-	//Initialize the velocity
-	mVelX = 0;
-	mVelY = 0;
-}
-
-void Dot::handleEvent(SDL_Event& e)
-{
-	//If a key was pressed
-	if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+	//Lock texture
+	else
 	{
-		//Adjust the velocity
-		switch (e.key.keysym.sym)
+		if (SDL_LockTexture(mTexture, NULL, &mPixels, &mPitch) != 0)
 		{
-		case SDLK_UP: mVelY -= DOT_VEL; break;
-		case SDLK_DOWN: mVelY += DOT_VEL; break;
-		case SDLK_LEFT: mVelX -= DOT_VEL; break;
-		case SDLK_RIGHT: mVelX += DOT_VEL; break;
+			printf("Unable to lock texture! %s\n", SDL_GetError());
+			success = false;
 		}
 	}
-	//If a key was released
-	else if (e.type == SDL_KEYUP && e.key.repeat == 0)
-	{
-		//Adjust the velocity
-		switch (e.key.keysym.sym)
-		{
-		case SDLK_UP: mVelY += DOT_VEL; break;
-		case SDLK_DOWN: mVelY -= DOT_VEL; break;
-		case SDLK_LEFT: mVelX += DOT_VEL; break;
-		case SDLK_RIGHT: mVelX -= DOT_VEL; break;
-		}
-	}
+
+	return success;
 }
 
-void Dot::move(Tile *tiles[])
+bool LTexture::unlockTexture()
 {
-	//Move the dot left or right
-	mBox.x += mVelX;
+	bool success = true;
 
-	//If the dot went too far to the left or right or touched a wall
-	if ((mBox.x < 0) || (mBox.x + DOT_WIDTH > LEVEL_WIDTH) || touchesWall(mBox, tiles))
+	//Texture is not locked
+	if (mPixels == NULL)
 	{
-		//move back
-		mBox.x -= mVelX;
+		printf("Texture is not locked!\n");
+		success = false;
+	}
+	//Unlock texture
+	else
+	{
+		SDL_UnlockTexture(mTexture);
+		mPixels = NULL;
+		mPitch = 0;
 	}
 
-	//Move the dot up or down
-	mBox.y += mVelY;
-
-	//If the dot went too far up or down or touched a wall
-	if ((mBox.y < 0) || (mBox.y + DOT_HEIGHT > LEVEL_HEIGHT) || touchesWall(mBox, tiles))
-	{
-		//move back
-		mBox.y -= mVelY;
-	}
+	return success;
 }
 
-void Dot::setCamera(SDL_Rect& camera)
+void* LTexture::getPixels()
 {
-	//Center the camera over the dot
-	camera.x = (mBox.x + DOT_WIDTH / 2) - SCREEN_WIDTH / 2;
-	camera.y = (mBox.y + DOT_HEIGHT / 2) - SCREEN_HEIGHT / 2;
-
-	//Keep the camera in bounds
-	if (camera.x < 0)
-	{
-		camera.x = 0;
-	}
-	if (camera.y < 0)
-	{
-		camera.y = 0;
-	}
-	if (camera.x > LEVEL_WIDTH - camera.w)
-	{
-		camera.x = LEVEL_WIDTH - camera.w;
-	}
-	if (camera.y > LEVEL_HEIGHT - camera.h)
-	{
-		camera.y = LEVEL_HEIGHT - camera.h;
-	}
+	return mPixels;
 }
 
-void Dot::render(SDL_Rect& camera)
+int LTexture::getPitch()
 {
-	//Show the dot
-	gDotTexture.render(mBox.x - camera.x, mBox.y - camera.y);
+	return mPitch;
 }
 
 bool init()
@@ -500,50 +362,56 @@ bool init()
 	return success;
 }
 
-bool loadMedia(Tile* tiles[])
+bool loadMedia()
 {
 	//Loading success flag
 	bool success = true;
 
-	//Load dot texture
-	if (!gDotTexture.loadFromFile("assets/dot.bmp"))
+	//Load foo' texture
+	if (!gFooTexture.loadFromFile("assets/foo.png"))
 	{
-		printf("Failed to load dot texture!\n");
+		printf("Failed to load corner texture!\n");
 		success = false;
 	}
-
-	//Load tile texture
-	if (!gTileTexture.loadFromFile("assets/tiles.png"))
+	else
 	{
-		printf("Failed to load tile set texture!\n");
-		success = false;
-	}
+		//Lock texture
+		if (!gFooTexture.lockTexture())
+		{
+			printf("Unable to lock Foo' texture!\n");
+		}
+		//Manual color key
+		else
+		{
+			//Get pixel data
+			Uint32* pixels = (Uint32*)gFooTexture.getPixels();
+			int pixelCount = (gFooTexture.getPitch() / 4) * gFooTexture.getHeight();
 
-	//Load tile map
-	if (!setTiles(tiles))
-	{
-		printf("Failed to load tile set!\n");
-		success = false;
+			//Map colors
+			Uint32 colorKey = SDL_MapRGB(SDL_GetWindowSurface(gWindow)->format, 0, 0xFF, 0xFF);
+			Uint32 transparent = SDL_MapRGBA(SDL_GetWindowSurface(gWindow)->format, 0xFF, 0xFF, 0xFF, 0x00);
+
+			//Color key pixels
+			for (int i = 0; i < pixelCount; ++i)
+			{
+				if (pixels[i] == colorKey)
+				{
+					pixels[i] = transparent;
+				}
+			}
+
+			//Unlock texture
+			gFooTexture.unlockTexture();
+		}
 	}
 
 	return success;
 }
 
-void close(Tile* tiles[])
+void close()
 {
-	//Deallocate tiles
-	for (int i = 0; i < TOTAL_TILES; ++i)
-	{
-		if (tiles[i] == NULL)
-		{
-			delete tiles[i];
-			tiles[i] = NULL;
-		}
-	}
-
 	//Free loaded images
-	gDotTexture.free();
-	gTileTexture.free();
+	gFooTexture.free();
 
 	//Destroy window	
 	SDL_DestroyRenderer(gRenderer);
@@ -556,208 +424,6 @@ void close(Tile* tiles[])
 	SDL_Quit();
 }
 
-bool checkCollision(SDL_Rect a, SDL_Rect b)
-{
-	//The sides of the rectangles
-	int leftA, leftB;
-	int rightA, rightB;
-	int topA, topB;
-	int bottomA, bottomB;
-
-	//Calculate the sides of rect A
-	leftA = a.x;
-	rightA = a.x + a.w;
-	topA = a.y;
-	bottomA = a.y + a.h;
-
-	//Calculate the sides of rect B
-	leftB = b.x;
-	rightB = b.x + b.w;
-	topB = b.y;
-	bottomB = b.y + b.h;
-
-	//If any of the sides from A are outside of B
-	if (bottomA <= topB)
-	{
-		return false;
-	}
-
-	if (topA >= bottomB)
-	{
-		return false;
-	}
-
-	if (rightA <= leftB)
-	{
-		return false;
-	}
-
-	if (leftA >= rightB)
-	{
-		return false;
-	}
-
-	//If none of the sides from A are outside B
-	return true;
-}
-
-bool setTiles(Tile* tiles[])
-{
-	//Success flag
-	bool tilesLoaded = true;
-
-	//The tile offsets
-	int x = 0, y = 0;
-
-	//Open the map
-	std::ifstream map("assets/lazy.map");
-
-	//If the map couldn't be loaded
-	if (!map)
-	{
-		printf("Unable to load map file!\n");
-		tilesLoaded = false;
-	}
-	else
-	{
-		//Initialize the tiles
-		for (int i = 0; i < TOTAL_TILES; ++i)
-		{
-			//Determines what kind of tile will be made
-			int tileType = -1;
-
-			//Read tile from map file
-			map >> tileType;
-
-			//If the was a problem in reading the map
-			if (map.fail())
-			{
-				//Stop loading map
-				printf("Error loading map: Unexpected end of file!\n");
-				tilesLoaded = false;
-				break;
-			}
-
-			//If the number is a valid tile number
-			if ((tileType >= 0) && (tileType < TOTAL_TILE_SPRITES))
-			{
-				tiles[i] = new Tile(x, y, tileType);
-			}
-			//If we don't recognize the tile type
-			else
-			{
-				//Stop loading map
-				printf("Error loading map: Invalid tile type at %d!\n", i);
-				tilesLoaded = false;
-				break;
-			}
-
-			//Move to next tile spot
-			x += TILE_WIDTH;
-
-			//If we've gone too far
-			if (x >= LEVEL_WIDTH)
-			{
-				//Move back
-				x = 0;
-
-				//Move to the next row
-				y += TILE_HEIGHT;
-			}
-		}
-
-		//Clip the sprite sheet
-		if (tilesLoaded)
-		{
-			gTileClips[TILE_RED].x = 0;
-			gTileClips[TILE_RED].y = 0;
-			gTileClips[TILE_RED].w = TILE_WIDTH;
-			gTileClips[TILE_RED].h = TILE_HEIGHT;
-
-			gTileClips[TILE_GREEN].x = 0;
-			gTileClips[TILE_GREEN].y = 80;
-			gTileClips[TILE_GREEN].w = TILE_WIDTH;
-			gTileClips[TILE_GREEN].h = TILE_HEIGHT;
-
-			gTileClips[TILE_BLUE].x = 0;
-			gTileClips[TILE_BLUE].y = 160;
-			gTileClips[TILE_BLUE].w = TILE_WIDTH;
-			gTileClips[TILE_BLUE].h = TILE_HEIGHT;
-
-			gTileClips[TILE_TOPLEFT].x = 80;
-			gTileClips[TILE_TOPLEFT].y = 0;
-			gTileClips[TILE_TOPLEFT].w = TILE_WIDTH;
-			gTileClips[TILE_TOPLEFT].h = TILE_HEIGHT;
-
-			gTileClips[TILE_LEFT].x = 80;
-			gTileClips[TILE_LEFT].y = 80;
-			gTileClips[TILE_LEFT].w = TILE_WIDTH;
-			gTileClips[TILE_LEFT].h = TILE_HEIGHT;
-
-			gTileClips[TILE_BOTTOMLEFT].x = 80;
-			gTileClips[TILE_BOTTOMLEFT].y = 160;
-			gTileClips[TILE_BOTTOMLEFT].w = TILE_WIDTH;
-			gTileClips[TILE_BOTTOMLEFT].h = TILE_HEIGHT;
-
-			gTileClips[TILE_TOP].x = 160;
-			gTileClips[TILE_TOP].y = 0;
-			gTileClips[TILE_TOP].w = TILE_WIDTH;
-			gTileClips[TILE_TOP].h = TILE_HEIGHT;
-
-			gTileClips[TILE_CENTER].x = 160;
-			gTileClips[TILE_CENTER].y = 80;
-			gTileClips[TILE_CENTER].w = TILE_WIDTH;
-			gTileClips[TILE_CENTER].h = TILE_HEIGHT;
-
-			gTileClips[TILE_BOTTOM].x = 160;
-			gTileClips[TILE_BOTTOM].y = 160;
-			gTileClips[TILE_BOTTOM].w = TILE_WIDTH;
-			gTileClips[TILE_BOTTOM].h = TILE_HEIGHT;
-
-			gTileClips[TILE_TOPRIGHT].x = 240;
-			gTileClips[TILE_TOPRIGHT].y = 0;
-			gTileClips[TILE_TOPRIGHT].w = TILE_WIDTH;
-			gTileClips[TILE_TOPRIGHT].h = TILE_HEIGHT;
-
-			gTileClips[TILE_RIGHT].x = 240;
-			gTileClips[TILE_RIGHT].y = 80;
-			gTileClips[TILE_RIGHT].w = TILE_WIDTH;
-			gTileClips[TILE_RIGHT].h = TILE_HEIGHT;
-
-			gTileClips[TILE_BOTTOMRIGHT].x = 240;
-			gTileClips[TILE_BOTTOMRIGHT].y = 160;
-			gTileClips[TILE_BOTTOMRIGHT].w = TILE_WIDTH;
-			gTileClips[TILE_BOTTOMRIGHT].h = TILE_HEIGHT;
-		}
-	}
-
-	//Close the file
-	map.close();
-
-	//If the map was loaded fine
-	return tilesLoaded;
-}
-
-bool touchesWall(SDL_Rect box, Tile* tiles[])
-{
-	//Go through the tiles
-	for (int i = 0; i < TOTAL_TILES; ++i)
-	{
-		//If the tile is a wall type tile
-		if ((tiles[i]->getType() >= TILE_CENTER) && (tiles[i]->getType() <= TILE_TOPLEFT))
-		{
-			//If the collision box touches the wall tile
-			if (checkCollision(box, tiles[i]->getBox()))
-			{
-				return true;
-			}
-		}
-	}
-
-	//If no wall tiles were touched
-	return false;
-}
-
 int main(int argc, char* args[])
 {
 	//Start up SDL and create window
@@ -767,11 +433,8 @@ int main(int argc, char* args[])
 	}
 	else
 	{
-		//The level tiles
-		Tile* tileSet[TOTAL_TILES];
-
 		//Load media
-		if (!loadMedia(tileSet))
+		if (!loadMedia())
 		{
 			printf("Failed to load media!\n");
 		}
@@ -782,12 +445,6 @@ int main(int argc, char* args[])
 
 			//Event handler
 			SDL_Event e;
-
-			//The dot that will be moving around on the screen
-			Dot dot;
-
-			//Level camera
-			SDL_Rect camera = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 
 			//While application is running
 			while (!quit)
@@ -800,36 +457,23 @@ int main(int argc, char* args[])
 					{
 						quit = true;
 					}
-
-					//Handle input for the dot
-					dot.handleEvent(e);
 				}
-
-				//Move the dot
-				dot.move(tileSet);
-				dot.setCamera(camera);
 
 				//Clear screen
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
 
-				//Render level
-				for (int i = 0; i < TOTAL_TILES; ++i)
-				{
-					tileSet[i]->render(camera);
-				}
-
-				//Render dot
-				dot.render(camera);
+				//Render stick figure
+				gFooTexture.render((SCREEN_WIDTH - gFooTexture.getWidth()) / 2, (SCREEN_HEIGHT - gFooTexture.getHeight()) / 2);
 
 				//Update screen
 				SDL_RenderPresent(gRenderer);
 			}
 		}
-
-		//Free resources and close SDL
-		close(tileSet);
 	}
+
+	//Free resources and close SDL
+	close();
 
 	return 0;
 }
